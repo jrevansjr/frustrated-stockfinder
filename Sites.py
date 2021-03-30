8c86717d927fcaa76eea820a3e88efbe9ddc5592
Johnny Evans <81573322+jrevansjr@users.noreply.github.com>
2021-03-29 19:19:58 -0500

    Add files via upload

---
+++ Sites.py
@@ -0,0 +1,173 @@
+sites = [
+    	    {
+                "item": "series x",
+                "source": "microsoft",
+                "url": "https://www.microsoft.com/en-us/p/xbox-series-x/8wj714n3rbtl?activetab=pivot%3aoverviewtab",
+                "type": "selenium",
+                "selector": "id",
+                "target": "buttons_ConfigureDeviceButton"
+            },            
+     	    {
+                "item": "series x",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/microsoft-xbox-series-x-1tb-console-black/6428324.p?skuId=6428324',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3070",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6429442',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3070 Gigabyte Eagle",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3070-eagle-8gb-gddr6-pci-express-4-0-graphics-card/6437912.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6437912',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3070 Gigabyte Gaming OC",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/pny-geforce-rtx-3070-8gb-xlr8-gaming-epic-x-rgb-triple-fan-graphics-card/6432653.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6432653',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3070 Evga",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/evga-geforce-rtx-3070-xc3-black-gaming-8gb-gddr6-pci-express-4-0-graphics-card/6439300.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6439300',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3070 PNY df",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/pny-geforce-rtx-3070-8gb-dual-fan-graphics-card/6432654.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6432654',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3070 PNY tf",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/pny-geforce-rtx-3070-8gb-xlr8-gaming-epic-x-rgb-triple-fan-graphics-card/6432653.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6432653',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080 Asus",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/asus-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-strix-graphics-card-black/6432445.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6432445',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080 Msi",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/msi-nvidia-geforce-rtx-3080-ventus-3x-10g-oc-bv-gddr6x-pci-express-4-0-graphic-card-black-silver/6430175.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6430175',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080 Gigabyte Eagle",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-eagle-oc-10gb-gddr6x-pci-express-4-0-graphics-card/6430621.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6430621',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080 Gigabyte Gaming",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-gaming-oc-10gb-gddr6x-pci-express-4-0-graphics-card/6430620.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6430620',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080 Gigabyte Aorus",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/gigabyte-nvidia-geforce-rtx-3080-aorus-master-10gb-gddr6x-pci-express-4-0-graphics-card/6436223.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6436223',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080 Evga",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/evga-geforce-rtx-3080-xc3-black-gaming-10gb-gddr6-pci-express-4-0-graphics-card/6432399.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6432399',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080 Evga Ultra",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/evga-geforce-rtx-3080-xc3-ultra-gaming-10gb-gddr6-pci-express-4-0-graphics-card/6432400.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6432400',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080 Pny",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/pny-geforce-rtx-3080-10gb-xlr8-gaming-epic-x-rgb-triple-fan-graphics-card/6432658.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6432658',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "gtx 3080 Pny XLR",
+                "source": "best buy",
+                "url": 'https://www.bestbuy.com/site/pny-geforce-rtx-3080-10gb-xlr8-gaming-epic-x-rgb-triple-fan-graphics-card/6432655.p?acampID=0&cmp=RMX&loc=Hatch&ref=198&skuId=6432655',
+                "type": "selenium",
+                "selector": "css",
+                "target": "button.add-to-cart-button"
+            },
+            {
+                "item": "series x",
+                "source": "walmart",
+                "url": "https://www.walmart.com/ip/Xbox-Series-X/443574645",
+                "type": "api",
+                "selector": "id",
+                "section": "product-overview",
+                "target": "Add to cart"
+            },
+            {
+                "item": "series x",
+                "source": "target",
+                "url": "https://www.target.com/p/xbox-series-x-console/-/A-80790841",
+                "type": "api",
+                "selector": "id",
+                "section": "root", 
+                "target": ["Pick up","In stock"]
+            },
+            {
+                "item": "gtx 3080 Evga",
+                "source": "new egg",
+                "url": "https://www.newegg.com/evga-geforce-rtx-3080-10g-p5-3881-kr/p/N82E16814487522?cm_mmc=vendor-nvidia&u1=s1616887559876oshsa53055",
+                "type": "api",
+                "selector": "css",
+                "section": "product-buy-box", 
+                "target": "Add to cart"
+            }
+        ]
---
+++ xsxFinderForMe.py
@@ -0,0 +1,139 @@
+#Libraries from internet example, although I don't utilize implicit wait, I have libraries commented out below for potential future development
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+import random
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from pushover import Client
+from xsxDataDict import clientName, tokenName, headers, driverPath
+from Sites import sites
+
+client = Client(clientName, api_token=tokenName)
+
+class Stockfinder:
+    def __init__(self):
+        # chose not to utilize a proxy or various chrome options but have listed below potential options
+        # PROXY = "socks5://IP Adress"
+        chrome_options = Options()
+        chrome_options.add_argument("--incognito") 
+        # chrome_options.add_argument('--no-sandbox') 
+        # chrome_options.add_argument("--headless") can alternatively options.headless = True
+        self.driver = webdriver.Chrome(driverPath, options=chrome_options)
+
+    def selenium(self, source, url, target, selector):
+        # some websites block requests or don't explicitly list if item is sold out, selenium is method chosen for these websites
+        self.driver.get(url)
+        wait = WebDriverWait(self.driver, 5)
+        # close microsoft popup
+        if source == "microsoft":
+            self.closePop()
+        # some websites are easier to identify target by class rather than just id
+        if selector == "id":
+            try:
+                button = wait.until(EC.visibility_of_element_located((By.ID, target)))
+            except:
+                print("INCORRECT ID LOCATOR")
+                return "ERROR"
+        elif selector == "css":
+            try:
+                button = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, target)))
+            except:
+                print("INCORRECT CSS LOCATOR")
+                return "ERROR"
+        elif selector == "xpath":
+            try:
+                button = wait.until(EC.visibility_of_element_located((By.XPATH,target)))
+            except:
+                print("INCORRECT XPATH LOCATOR")
+                return "ERROR"
+        else:
+                print("SELECTOR ERROR")    
+                return "ERROR"
+        # checks if add to cart button is enabled
+        if button.is_enabled() == True:
+            return "IN STOCK"
+        else:
+            return "OUT OF STOCK"
+
+    def api(self, url, section, target, selector):
+        # some websites utilizes Captchas or explicitly list if an item is in stock. 
+        # need to spoof header to get specific product data.
+        page = requests.get(url, headers = headers)
+        pageSoup = BeautifulSoup(page.content, 'html.parser')
+        # some websites filtering product header is easier by css, section could be fleshed out better
+        if selector=="id":
+            results = pageSoup.find(id=section)
+        elif selector=="css":
+            results = pageSoup.find(class_=section)
+        else:
+            print("SELECTOR ERROR")
+            return "ERROR"
+        hits = 0
+        # some websites are not consistant and may utilize a variety of tags in their product header to indicate stock (eg. pick up, in stock, add to cart). Option to search for various targets
+        if type(target) == str:
+            target = [target]
+        # loops throughh all potential targets to find a hit
+        for i in target:
+            hits = hits + int(i in results.getText())
+        if hits > 0:
+            return "IN STOCK"
+        else:
+            return "OUT OF STOCK"
+        
+    def diagnostics(self, scrapeType, source, url):
+        # diagnostics to take snapshot of page to improve code
+        if scrapeType == "selenium":
+            diagnosticsText = self.driver.page_source
+        else:
+            diagnosticsText = requests.get(url,headers=headers).text
+        # saves file in USER folder
+        with open("diagnostics.htm",'w',encoding="utf-8") as f:
+            f.write(diagnosticsText)
+            f.close()
+
+    def sendMessage(self, link, location):
+        client.send_message(link, title=location)
+    
+    def closePop(self):
+        try:
+            wait = WebDriverWait(self.driver, 5)
+            wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@class='c-dialog f-flow']/div[@class='sfw-dialog']/div['c-glyph glyph-cancel']"))).click()
+        except:
+            pass
+    
+def main():
+    sf = Stockfinder()
+    
+    finder = False
+    while finder == False:
+        print('Current run time: ', datetime.now())
+        for site in sites:
+            #determines if selenium or requests library is needed to scrape website
+            if site.get("type") == "selenium":
+                returnStatus = sf.selenium(site.get("source"),site.get("url"),site.get("target"),site.get("selector"))
+            elif site.get("type") == "api":
+                returnStatus = sf.api(site.get("url"),site.get("section"),site.get("target"),site.get("selector"))
+            else:
+                returnStatus = "ERROR"
+           
+            #notifies user via pushover app if either function returns an in stock response
+            if returnStatus == "OUT OF STOCK":
+                print("no Stock: "+site.get("source") + ", "+site.get("item"))
+            elif returnStatus == "ERROR":
+                print("ERROR: "+site.get("source")+","+site.get("item")+","+site.get("url"))
+            else:
+                sf.sendMessage(site.get("url"), "IN STOCK: "+site.get("source").upper()+", "+site.get("item").upper())
+                print("IN STOCK: "+site.get("source").upper()+", "+site.get("item").upper())
+                sf.diagnostics(site.get("type"),site.get("source"),site.get("url"))
+                finder = True
+        # sleeps random interval between 30 and 90 seconds then restarts
+        time.sleep(random.uniform(30,120))
+    sf.driver.quit()
+
+if __name__ == "__main__":
+	main()
